f4f11e887a1e5b2f9f8d02c9db4475ebf43e68ed
AnshSharma-05 <sharmaji.ansh05032005@gmail.com>
2026-04-12 13:58:39 +0530

    feat: implement auth logic, role-based routing,and auth provider context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { useContext , useEffect , useState} from 'react'
 import Login from './components/auth/login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { setLocalStorage, getLocalStorage } from './utils/localStorage'
-import { useEffect } from 'react'
+import { AuthContext } from './context/AuthProvider'
 
-const App = () => {
 
-  useEffect(() => {
-    setLocalStorage();
-    getLocalStorage();
-  }, []);
+const App = () => {       
+  const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+  console.log(authData);
+
+  useEffect(()=>{
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    
+    if(loggedInUser){
+      const userData = JSON.parse(loggedInUser)
+      setUser(userData.role)
+      setLoggedInUserData(userData.data)
+    }
+
+  },[])
+  
+
+  const handleLogin = (email, password) => {
+    if(email == 'admin@me.com' && password == '123'){
+      setUser({email, role: 'admin'});
+      localStorage.setItem('loggedInUser', JSON.stringify({email, role: 'admin'}));
+    }else if(authData && authData.employees.find((e)=> e.email == email && e.password == password )){
+      setUser({email, role: 'employee'});
+      localStorage.setItem('loggedInUser', JSON.stringify({email, role: 'employee'}));
+    }else{
+      alert("Invalid credentials");  
+    }
+  }
+
+  
 
   return (
     <>
-     {/* <Login /> */}
-      <EmployeeDashboard /> 
-     {/* <AdminDashboard /> */}
+     {user == null ? <Login handleLogin={handleLogin} /> : user.role == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
